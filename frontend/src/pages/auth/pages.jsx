f826f907f0b01ad9163a646a8ef51f5fcc059743
MatheusLimaBarbosa <matheus.barbosa.lb@gmail.com>
2024-12-15 20:12:49 -0300

    Fazendo a interação do backend com o frontend nas paginas de login e profile

--- frontend/src/pages/auth/pages.jsx
+++ frontend/src/pages/auth/pages.jsx
@@ -1,5 +1,123 @@
+import { useState, useEffect } from "react";
+import { TextField, Button, CircularProgress } from "@mui/material";
+import styles from './page.module.css';
+import authServices from "../../services/auth";
+import { useNavigate } from "react-router-dom";
+
 export default function Auth() {
+    const [formType, setFormType] = useState('login');
+    const [formData, setFormData] = useState({});
+    const [errorMessage, setErrorMessage] = useState('');
+    const { login, signup, authLoading } = authServices();
+
+    const navigate = useNavigate();
+    const authData = JSON.parse(localStorage.getItem('auth'));
+
+    useEffect(() => {
+        if (authData) {
+            navigate('/profile');
+        }
+    }, [authData, navigate]);  
+
+    const handleChangeFormType = () => {
+        setFormData({});
+        setErrorMessage(''); 
+        setFormType(formType === 'login' ? 'signup' : 'login');
+    };
+
+    const handleFormDataChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        });
+    };
+
+    const handleSubmitForm = (e) => {
+        e.preventDefault();
+        setErrorMessage('');  
+
+        switch (formType) {
+            case 'login':
+                login(formData);
+                break;
+            case 'signup':
+                if (formData.password !== formData.confirmPassword) {
+                    setErrorMessage('Passwords do not match');
+                    return;
+                }
+                signup(formData);
+                break;
+        }
+    };
+
+    if (authLoading) {
+        return <CircularProgress />;
+    }
+
     return (
-        <h1>auth</h1>
-    )
+        <div className={styles.authPageContainer}>
+            {formType === 'login' && (
+                <>
+                    <h1>Login</h1>
+                    <Button onClick={handleChangeFormType}>Don't you have an account? Click here</Button>
+                    <form onSubmit={handleSubmitForm}>
+                        <TextField
+                            required
+                            label="Email"
+                            type="email"
+                            name="email"
+                            onChange={handleFormDataChange}
+                        />
+                        <TextField
+                            required
+                            label="Password"
+                            type="password"
+                            name="password"
+                            onChange={handleFormDataChange}
+                        />
+                        <Button type="submit" variant="contained">Login</Button>
+                    </form>
+                </>
+            )}
+
+            {formType === 'signup' && (
+                <>
+                    <h1>Signup</h1>
+                    <Button onClick={handleChangeFormType}>Already have an account? Click here</Button>
+                    <form onSubmit={handleSubmitForm}>
+                        <TextField
+                            required
+                            label="Fullname"
+                            type="text"
+                            name="fullname"
+                            onChange={handleFormDataChange}
+                        />
+                        <TextField
+                            required
+                            label="Email"
+                            type="email"
+                            name="email"
+                            onChange={handleFormDataChange}
+                        />
+                        <TextField
+                            required
+                            label="Password"
+                            type="password"
+                            name="password"
+                            onChange={handleFormDataChange}
+                        />
+                        <TextField
+                            required
+                            label="Confirm password"
+                            type="password"
+                            name="confirmPassword"
+                            onChange={handleFormDataChange}
+                        />
+                        <Button type="submit" variant="contained">Signup</Button>
+                    </form>
+                    {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
+                </>
+            )}
+        </div>
+    );
 }
--- frontend/src/pages/profile/pages.jsx
+++ frontend/src/pages/profile/pages.jsx
@@ -1,5 +1,66 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import authServices from "../../services/auth";
+import orderServices from "../../services/order";
+import styles from './page.module.css'
+
 export default function Profile() {
+    const { logout } = authServices();
+    const { getUserOrders, orderLoading, refetchOrders, ordersList } = orderServices()
+    const navigate = useNavigate();
+    const authData = JSON.parse(localStorage.getItem('auth'));
+
+    useEffect(() => {
+        if (!authData) {
+            navigate('/auth');
+        } else if(refetchOrders) {
+            getUserOrders(authData?.user?._id)
+        }
+    }, [authData, navigate, refetchOrders]);  
+
+    if(orderLoading) {
+        return(<h1>Loading...</h1>)
+    }
+
+    const handleLogout = () => {
+        logout();
+        navigate('/');  
+    };
+
+    console.log(ordersList)
+
+    if (!authData) {  
+        return null;
+    }
+
     return (
-        <h1>profile</h1>
-    )
+        <div className={styles.pageContainer}>
+            <div>
+                <h1>{authData?.user?.fullname}</h1>
+                <h3>{authData?.user?.email}</h3>
+            </div>
+            <button onClick={handleLogout}>Logout</button>
+
+            {ordersList.length > 0 ?
+                <div className={styles.ordersContainer}>
+                    {ordersList.map((order) => (
+                        <div key={order._id} className={styles.orderContainer}>
+                            <h3>{order.pickupStatus}</h3>
+                            <h3>{order.pickupTime}</h3>
+                            {order.orderItems.map((item) => (
+                                <div key={item._id}>
+                                    <h4>{item.itemDetails[0].name}</h4>
+                                    <p>Quantity: {item.quantity}</p>
+                                </div>
+                            ))}
+                        </div>
+                    ))}
+                </div>
+            :
+                <div>
+                    You do not have orders yet
+                </div>    
+            }
+        </div>
+    );
 }
